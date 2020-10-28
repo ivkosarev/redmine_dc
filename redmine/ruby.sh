d3f78020f945c385fb9ffae40ca2c1326952bc83
Ivan Kosarev <ivkosarev@gmail.com>
2020-10-28 15:10:09 +0300

    Try copmose

--- redmine/ruby.sh
+++ redmine/ruby.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash -s stable --ruby
-rvm install ruby
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+source /usr/local/rvm/scripts/rvm
 rvm use 2.6.5
 gem install bundler 
 bundle config set without 'development test rmagick'
 cd /opt/redmine-4.1 && bundle install
-bundle config set without 'development test rmagick'
 bundle exec rake generate_secret_token
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production REDMINE_LANG=ru bundle exec rake redmine:load_default_data
