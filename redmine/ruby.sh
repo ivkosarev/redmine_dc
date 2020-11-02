291c88fe94573a098c314f6a30ceafac53be9732
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-02 15:14:37 +0300

    try docker-compose

--- redmine/ruby.sh
+++ redmine/ruby.sh
@@ -3,7 +3,4 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 source /usr/local/rvm/scripts/rvm
 rvm install 2.6.5
-rvm use 2.6.5
-gem install bundler 
-bundle config set without 'development test rmagick'
-cd /opt/redmine-4.1 && bundle install
+
