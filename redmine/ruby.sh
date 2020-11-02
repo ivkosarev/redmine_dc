b02465f7b050e992200662c951216dcff478167e
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-02 14:55:31 +0300

    try docker-compose

--- redmine/ruby.sh
+++ redmine/ruby.sh
@@ -7,6 +7,3 @@ rvm use 2.6.5
 gem install bundler 
 bundle config set without 'development test rmagick'
 cd /opt/redmine-4.1 && bundle install
-bundle exec rake generate_secret_token
-RAILS_ENV=production bundle exec rake db:migrate
-RAILS_ENV=production REDMINE_LANG=ru bundle exec rake redmine:load_default_data
