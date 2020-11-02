cc4d61aca35aa0783b4b2fa6437cb152b670ae98
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-02 15:49:54 +0300

    try docker-compose

--- redmine/init.sh
+++ redmine/init.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+bundle config set without 'development test rmagick'
+bundle exec rake generate_secret_token
+RAILS_ENV=production bundle exec rake db:migrate
+RAILS_ENV=production REDMINE_LANG=ru bundle exec rake redmine:load_default_data
+rake rails:update:bin
 ruby bin/rails server -b 0.0.0.0 -e production
