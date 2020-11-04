a67cc5a58bf636e1563037ce571edc1fd23b5a14
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-04 20:28:56 +0300

    123

--- redmine/init.sh
+++ redmine/init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-source /usr/local/rvm/scripts/rvm
-rvm use 2.6.5
-gem install bundler
-cd /opt/redmine-4.1 && bundle install
-bundle config set without 'development test rmagick'
-bundle exec rake generate_secret_token
-RAILS_ENV=production bundle exec rake db:migrate --trace
-RAILS_ENV=production REDMINE_LANG=ru bundle exec rake redmine:load_default_data
-rake app:update:bin
+#source /usr/local/rvm/scripts/rvm
+#rvm use 2.6.5
+#gem install bundler
+#cd /opt/redmine-4.1 && bundle install
+##bundle config set without 'development test rmagick'
+#bundle exec rake generate_secret_token
+#RAILS_ENV=production bundle exec rake db:migrate --trace
+#RAILS_ENV=production REDMINE_LANG=ru bundle exec rake redmine:load_default_data
+#rake app:update:bin
 ruby bin/rails server -b 0.0.0.0 -e production
