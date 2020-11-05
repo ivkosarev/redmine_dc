9fca78d779e7ff68fe9fa626ee4d39a488b68a76
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-05 19:43:44 +0300

    123

--- redmine/install_r.sh
+++ redmine/install_r.sh
@@ -8,3 +8,4 @@ bundle exec rake generate_secret_token
 RAILS_ENV=production bundle exec rake db:migrate --trace
 RAILS_ENV=production REDMINE_LANG=ru bundle exec rake redmine:load_default_data
 rake app:update:bin
+ruby bin/rails server -b 0.0.0.0 -e production
