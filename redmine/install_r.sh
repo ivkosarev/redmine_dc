8ff94c23ee4087e7bcd8d84d8a5b88af62738003
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-05 19:57:46 +0300

    123

--- redmine/install_r.sh
+++ redmine/install_r.sh
@@ -7,5 +7,4 @@ bundle config set without 'development test rmagick'
 bundle exec rake generate_secret_token
 RAILS_ENV=production bundle exec rake db:migrate --trace
 RAILS_ENV=production REDMINE_LANG=ru bundle exec rake redmine:load_default_data
-rake app:update:bin
 ruby bin/rails server -b 0.0.0.0 -e production
