d96b150f90cd122a2fd25bbe9a79be609096cab4
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-06 12:07:26 +0300

    try install ruby from sourcecodes

--- redmine/ruby.sh
+++ redmine/ruby.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-curl -sSL https://get.rvm.io | bash -s stable --ruby
-source /usr/local/rvm/scripts/rvm
-rvm install 2.6.5
-rvm use 2.6.5
+cd /opt && wget https://cache.ruby-lang.org/pub/ruby/2.6/ruby-2.6.6.tar.gz
+tar -xvf ruby-2.6.6.tar.gz && cd ruby-2.6.6 && ./configure && make && make install
+gem install bundler
+cd /opt/redmine && bundle install
+bundle config set without 'development test rmagick'
+bundle exec rake generate_secret_token
+RAILS_ENV=production bundle exec rake db:migrate --trace
+RAILS_ENV=production REDMINE_LANG=ru bundle exec rake redmine:load_default_data
