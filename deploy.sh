8d32e05de1cd7079fd43d06a81bf20529ae35c72
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-06 20:09:14 +0300

    try travis

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 docker build -t  redm1ne ./redmine
 docker tag redm1ne ivkosarev/redm1ne:latest
@@ -10,3 +10,6 @@ docker push ivkosarev/redm1ne:latest
 if [ $? -eq 0 ]
 then
   echo "Buil Successfully Pushed!"
+else
+  echo "something went wrong" >&2
+fi
