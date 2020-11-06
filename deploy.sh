75dc713a345384d049f3250678d849ab4b92bd3b
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-06 18:47:23 +0300

    try travis

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,13 @@ docker tag redmine:${GIT_VERSION} redmine:latest
 
 # Вход в Docker Hub и выгрузка образа
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker push ${IMAGE}:${GIT_VERSION}
+docker push redmine:${GIT_VERSION}
 
 # Найти ID работающего контейнера
 CONTAINER_ID=$(docker ps | grep takenote | cut -d" " -f1)
 
 # Остановить старый контейнер, запустить новый, очистить систему
 docker stop ${CONTAINER_ID}
-docker run --restart unless-stopped -d -p 80:5000 ${IMAGE}:${GIT_VERSION}
+docker run --restart unless-stopped -d -p 80:3000 redmine:${GIT_VERSION}
 docker system prune -a -f
 docker push 
