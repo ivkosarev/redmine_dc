69f97bc9b4d69a0f2a548c6d135b23d83c3ab3f3
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-06 18:37:08 +0300

    try travis

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-IMAGE="<username>/<repository>"                             # Образ Docker
+
 GIT_VERSION=$(git describe --always --abbrev --tags --long) # Git-хэш и теги
 
 # Сборка и тегирование образа
-docker build -t ${IMAGE}:${GIT_VERSION} .
-docker tag ${IMAGE}:${GIT_VERSION} ${IMAGE}:latest
+docker build -t redmine:${GIT_VERSION} .
+docker tag redmine:${GIT_VERSION} redmine:latest
 
 # Вход в Docker Hub и выгрузка образа
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
@@ -19,3 +19,4 @@ CONTAINER_ID=$(docker ps | grep takenote | cut -d" " -f1)
 docker stop ${CONTAINER_ID}
 docker run --restart unless-stopped -d -p 80:5000 ${IMAGE}:${GIT_VERSION}
 docker system prune -a -f
+docker push 
