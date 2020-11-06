747cde617196bcfa121e6d734bae0f359f9b61a0
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-06 13:10:17 +0300

    try install ruby with 1 script

--- redmine/ruby.sh
+++ redmine/ruby.sh
@@ -7,5 +7,3 @@ gem install bundler
 bundle install
 bundle config set without 'development test rmagick'
 bundle exec rake generate_secret_token
-RAILS_ENV=production bundle exec rake db:migrate --trace
-RAILS_ENV=production REDMINE_LANG=ru bundle exec rake redmine:load_default_data
