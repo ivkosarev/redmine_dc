d9eeeb3a6443f36007a505638cfc73bb22a233f3
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-06 19:58:39 +0300

    try travis

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,12 @@ set -e
 GIT_VERSION=$(git describe --always --abbrev --tags --long) # Git-хэш и теги
 
 # Сборка и тегирование образа
-docker build -t  redmine ./redmine
+docker build -t  redm1ne ./redmine
+docker tag redm1ne ivkosarev/redm1ne:latest
 
 # Вход в Docker Hub и выгрузка образа
 docker login --username ivkosarev -p 9f2d46ec-251b-46e9-89ef-619d99477ef4
-docker push docker push ivkosarev/tst_repo:redmine
+docker push ivkosarev/redm1ne:latest
 
 # Найти ID работающего контейнера
 CONTAINER_ID=$(docker ps | grep takenote | cut -d" " -f1)
