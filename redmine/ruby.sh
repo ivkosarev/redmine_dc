c00c74bc0eb8a36432b747066e0d6c6879460a26
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-06 12:24:28 +0300

    try install ruby from sourcecodes

--- redmine/ruby.sh
+++ redmine/ruby.sh
@@ -3,7 +3,7 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 source /usr/local/rvm/scripts/rvm
 #rvm use 2.6.5
 gem install bundler
-cd /opt/redmine-4.1 && bundle install
+cd /opt/redmine && bundle install
 bundle config set without 'development test rmagick'
 bundle exec rake generate_secret_token
 RAILS_ENV=production bundle exec rake db:migrate --trace
