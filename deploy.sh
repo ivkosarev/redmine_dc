25e422efc63df53a5f1e40e46b3c1e283c39943e
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-06 17:17:53 +0300

    try travis

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+IMAGE="<username>/<repository>"                             # Образ Docker
 GIT_VERSION=$(git describe --always --abbrev --tags --long) # Git-хэш и теги
 
 # Сборка и тегирование образа
@@ -10,3 +11,11 @@ docker tag ${IMAGE}:${GIT_VERSION} ${IMAGE}:latest
 # Вход в Docker Hub и выгрузка образа
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push ${IMAGE}:${GIT_VERSION}
+
+# Найти ID работающего контейнера
+CONTAINER_ID=$(docker ps | grep takenote | cut -d" " -f1)
+
+# Остановить старый контейнер, запустить новый, очистить систему
+docker stop ${CONTAINER_ID}
+docker run --restart unless-stopped -d -p 80:5000 ${IMAGE}:${GIT_VERSION}
+docker system prune -a -f
